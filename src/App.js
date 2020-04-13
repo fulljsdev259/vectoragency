e5d808b934789e8c4bb4fa9005b2775618c0a623
fulljsdev259 <mohdd259@gmail.com>
2020-04-13 11:44:21 +0530

    removed unused packages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
-import Meetings from "./screens/Meetings";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import AddMeeting from "./screens/AddMeeting";
 import Routes from "./routes";
 
 function App() {
